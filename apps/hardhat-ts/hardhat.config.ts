60f29d9945621e35800a8e959f440a6e7d2d6e08
Rok Erjavec <rok.erjavec@spica.com>
2022-08-21 23:46:03 +0200

    Adds dotenv to hardhat project

--- apps/hardhat-ts/hardhat.config.ts
+++ apps/hardhat-ts/hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig();
+
+const { ALCHEMY_API_KEY, GOERLI_PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY ?? ''],
+    },
+  },
 };
 
 export default config;
